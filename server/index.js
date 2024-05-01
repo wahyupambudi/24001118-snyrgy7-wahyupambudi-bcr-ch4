99f99ea730c2ed6430dda28b04e2f7b018c0080f
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-05-01 21:54:58 +0700

    feat(bcr) - add routing, and fix path file

--- server/index.js
+++ server/index.js
@@ -1,41 +1,51 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
+// membuat fungsi server
 const server = http.createServer((req, res) => {
-    let filePath = '.' + req.url;
-    if (filePath === './') {
-        filePath = './public/index.html'; // Redirect to index.html
-    }
+  //   mendapatkan filepath dari url
+  let filePath = "." + req.url;
+  //   console.log(filePath);
+  //   routing
+  if (filePath === "./") {
+    filePath = "./public/index.html"; // Redirect to index.html
+  } else if (filePath === "./rental") {
+    filePath = "./public/rental-car.html";
+  }
 
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
-    if (extname === '.css') {
-        contentType = 'text/css';
-    } else if (extname === '.js') {
-        contentType = 'text/javascript';
-    }
+  //   mendapatkan extensi dari file yang didapatkan pada filepath url
+  //   untuk kebutuhan mime
+  const extname = path.extname(filePath);
+  console.log(extname);
+  let contentType = "text/html";
+  if (extname === ".css") {
+    contentType = "text/css";
+  } else if (extname === ".js") {
+    contentType = "text/javascript";
+  }
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                // File not found
-                res.writeHead(404);
-                res.end('File not found');
-            } else {
-                // Server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
-            // Success
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
+  //   proses membaca file dari filepath dan pengkondisian jika 404, dan 500
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // File not found
+        res.writeHead(404);
+        res.end("File not found");
+      } else {
+        // Server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
