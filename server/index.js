e20c9acfac13d0132b3841827f76fe42506fee0b
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-05-01 21:31:21 +0700

    feat(bcr) - create server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,41 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './public/index.html'; // Redirect to index.html
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    if (extname === '.css') {
+        contentType = 'text/css';
+    } else if (extname === '.js') {
+        contentType = 'text/javascript';
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // File not found
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                // Server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
